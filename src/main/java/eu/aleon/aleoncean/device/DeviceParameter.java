d45492abd72ba14f764077b67754e37dc673b710
Markus Rathgeb <maggu2810@gmail.com>
2015-03-02 15:30:21 +0100

    improve internal DeviceParameter lookup

--- src/main/java/eu/aleon/aleoncean/device/DeviceParameter.java
+++ src/main/java/eu/aleon/aleoncean/device/DeviceParameter.java
@@ -15,9 +15,9 @@
  */
 package eu.aleon.aleoncean.device;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 import eu.aleon.aleoncean.values.RockerSwitchAction;
 import eu.aleon.aleoncean.values.WindowHandlePosition;
 
@@ -48,127 +48,72 @@ import eu.aleon.aleoncean.values.WindowHandlePosition;
  */
 public enum DeviceParameter {
 
-    BUTTON_DIM_A,
-    BUTTON_DIM_B,
-    ENERGY_WS,
-    HUMIDITY_PERCENT,
-    ILLUMINATION_LUX,
-    MOTION,
-    OCCUPANCY_BUTTON,
-    POSITION_PERCENT,
-    POWER_W,
-    SETPOINT_POSITION_PERCENT,
-    SETPOINT_TEMPERATURE_CELSIUS,
-    SUPPLY_VOLTAGE_V,
-    SWITCH,
-    TEMPERATURE_CELSIUS,
-    TEMPERATURE_CONTROL_ENABLE,
-    TEMPERATURE_CONTROL_CUR_TEMP,
-    WINDOW_HANDLE_POSITION,
+    BUTTON_DIM_A("BUTTON_DIM_A", RockerSwitchAction.class),
+    BUTTON_DIM_B("BUTTON_DIM_B", RockerSwitchAction.class),
+    ENERGY_WS("ENERGY_WS", Long.class),
+    HUMIDITY_PERCENT("HUMIDITY_PERCENT", Double.class),
+    ILLUMINATION_LUX("ILLUMINATION_LUX", Double.class),
+    MOTION("MOTION", Boolean.class),
+    OCCUPANCY_BUTTON("OCCUPANCY_BUTTON", Boolean.class),
+    POSITION_PERCENT(Integer.class),
+    POWER_W(Long.class),
+    SETPOINT_POSITION_PERCENT(Integer.class),
+    SETPOINT_TEMPERATURE_CELSIUS(Double.class),
+    SUPPLY_VOLTAGE_V("SUPPLY_VOLTAGE_V", Double.class),
+    SWITCH("SWITCH", Boolean.class),
+    TEMPERATURE_CELSIUS("TEMPERATURE_CELSIUS", Double.class),
+    TEMPERATURE_CONTROL_ENABLE(Boolean.class),
+    TEMPERATURE_CONTROL_CUR_TEMP(Double.class),
+    WINDOW_HANDLE_POSITION("WINDOW_HANDLE_POSITION", WindowHandlePosition.class),
 
-    TMP_RECV_SERVICE_ON,
-    TMP_RECV_ENERGY_INPUT_ENABLED,
-    TMP_RECV_ENERGY_STORAGE_SUFFICIENT,
-    TMP_RECV_CHANGE_BATTERY,
-    TMP_RECV_COVER_OPEN,
-    TMP_RECV_TEMPERATURE_SENSOR_FAILURE,
-    TMP_RECV_WINDOW_OPEN,
-    TMP_RECV_ACTUATOR_OBSTRUCTED,
-    TMP_SEND_RUN_INIT_SEQUENCE,
-    TMP_SEND_LIFT_SET,
-    TMP_SEND_VALVE_OPEN,
-    TMP_SEND_VALVE_CLOSED,
-    TMP_SEND_REDUCED_ENERGY_CONSUMPTION;
+    /* temporary workaround to force sending configuration to device */
+    TMP_SEND_CONFIGURATION(Boolean.class),
 
-    private static class Info {
+    TMP_RECV_SERVICE_ON(Boolean.class),
+    TMP_RECV_ENERGY_INPUT_ENABLED(Boolean.class),
+    TMP_RECV_ENERGY_STORAGE_SUFFICIENT(Boolean.class),
+    TMP_RECV_CHANGE_BATTERY(Boolean.class),
+    TMP_RECV_COVER_OPEN(Boolean.class),
+    TMP_RECV_TEMPERATURE_SENSOR_FAILURE(Boolean.class),
+    TMP_RECV_WINDOW_OPEN(Boolean.class),
+    TMP_RECV_ACTUATOR_OBSTRUCTED(Boolean.class),
+    TMP_SEND_RUN_INIT_SEQUENCE(Boolean.class),
+    TMP_SEND_LIFT_SET(Boolean.class),
+    TMP_SEND_VALVE_OPEN(Boolean.class),
+    TMP_SEND_VALVE_CLOSED(Boolean.class),
+    TMP_SEND_REDUCED_ENERGY_CONSUMPTION(Boolean.class);
 
-        private final DeviceParameter parameter;
-        private final Class<?> clazz;
-        private final String name;
+    private final Class<?> clazz;
+    private final String userName;
 
-        public Info(final DeviceParameter parameter,
-                    final Class<?> clazz) {
-            this(parameter, parameter.name(), clazz);
-        }
-
-        public Info(final DeviceParameter parameter,
-                    final String name,
-                    final Class<?> clazz) {
-            this.parameter = parameter;
-            this.name = name;
-            this.clazz = clazz;
-        }
-
-        public DeviceParameter getParameter() {
-            return parameter;
-        }
-
-        public Class<?> getSupportedClass() {
-            return clazz;
-        }
-
-        public String getName() {
-            return name;
-        }
+    private DeviceParameter(final Class<?> clazz) {
+        this(null, clazz);
     }
 
-    private static class Infos {
-
-        private static final List<Info> INFOS = Collections.unmodifiableList(Arrays
-                .asList(
-                        new Info(DeviceParameter.BUTTON_DIM_A, "BUTTON_DIM_A", RockerSwitchAction.class),
-                        new Info(DeviceParameter.BUTTON_DIM_B, "BUTTON_DIM_B", RockerSwitchAction.class),
-                        new Info(DeviceParameter.ENERGY_WS, "ENERGY_WS", Long.class),
-                        new Info(DeviceParameter.HUMIDITY_PERCENT, "HUMIDITY_PERCENT", Double.class),
-                        new Info(DeviceParameter.ILLUMINATION_LUX, "ILLUMINATION_LUX", Double.class),
-                        new Info(DeviceParameter.MOTION, "MOTION", Boolean.class),
-                        new Info(DeviceParameter.OCCUPANCY_BUTTON, "OCCUPANCY_BUTTON", Boolean.class),
-                        new Info(DeviceParameter.POSITION_PERCENT, Integer.class),
-                        new Info(DeviceParameter.POWER_W, "POWER_W", Long.class),
-                        new Info(DeviceParameter.SETPOINT_POSITION_PERCENT, Integer.class),
-                        new Info(DeviceParameter.SETPOINT_TEMPERATURE_CELSIUS, Double.class),
-                        new Info(DeviceParameter.SUPPLY_VOLTAGE_V, "SUPPLY_VOLTAGE_V", Double.class),
-                        new Info(DeviceParameter.SWITCH, "SWITCH", Boolean.class),
-                        new Info(DeviceParameter.TEMPERATURE_CELSIUS, "TEMPERATURE_CELSIUS", Double.class),
-                        new Info(DeviceParameter.TEMPERATURE_CONTROL_CUR_TEMP, Double.class),
-                        new Info(DeviceParameter.TEMPERATURE_CONTROL_ENABLE, Boolean.class),
-                        new Info(DeviceParameter.WINDOW_HANDLE_POSITION, "WINDOW_HANDLE_POSITION", WindowHandlePosition.class),
-                        new Info(DeviceParameter.TMP_RECV_SERVICE_ON, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_ENERGY_INPUT_ENABLED, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_ENERGY_STORAGE_SUFFICIENT, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_CHANGE_BATTERY, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_COVER_OPEN, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_TEMPERATURE_SENSOR_FAILURE, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_WINDOW_OPEN, Boolean.class),
-                        new Info(DeviceParameter.TMP_RECV_ACTUATOR_OBSTRUCTED, Boolean.class),
-                        new Info(DeviceParameter.TMP_SEND_RUN_INIT_SEQUENCE, Boolean.class),
-                        new Info(DeviceParameter.TMP_SEND_LIFT_SET, Boolean.class),
-                        new Info(DeviceParameter.TMP_SEND_VALVE_OPEN, Boolean.class),
-                        new Info(DeviceParameter.TMP_SEND_VALVE_CLOSED, Boolean.class),
-                        new Info(DeviceParameter.TMP_SEND_REDUCED_ENERGY_CONSUMPTION, Boolean.class)
-                ));
-
-        public static Info getByParameter(final DeviceParameter parameter) {
-            for (final Info info : INFOS) {
-                if (info.getParameter().equals(parameter)) {
-                    return info;
-                }
-            }
-            return null;
+    private DeviceParameter(final String userName, final Class<?> clazz) {
+        if (userName == null) {
+            this.userName = name();
+        } else {
+            this.userName = userName;
         }
+        this.clazz = clazz;
+    }
 
-        public static Info getByName(final String name) {
-            for (final Info info : INFOS) {
-                if (info.getName().equals(name)) {
-                    return info;
-                }
-            }
-            return null;
+    private Class<?> getSupportedClass() {
+        return clazz;
+    }
+
+    private String getUserName() {
+        return userName;
+    }
+
+    private static final Map<String, DeviceParameter> nameToParameterMapping;
+
+    static {
+        nameToParameterMapping = new HashMap<>();
+        for (final DeviceParameter param : DeviceParameter.values()) {
+            nameToParameterMapping.put(param.getUserName(), param);
         }
-
-        private Infos() {
-        }
-
     }
 
     /**
@@ -178,21 +123,11 @@ public enum DeviceParameter {
      * @return Return a device parameter if the string is known, null if it is unknown.
      */
     public static DeviceParameter fromString(final String parameter) {
-        final Info info = Infos.getByName(parameter);
-        if (info != null) {
-            return info.getParameter();
-        } else {
-            return null;
-        }
+        return nameToParameterMapping.get(parameter);
     }
 
     public static Class<?> getSupportedClass(final DeviceParameter parameter) throws IllegalDeviceParameterException {
-        final Info info = Infos.getByParameter(parameter);
-        if (info != null) {
-            return info.getSupportedClass();
-        } else {
-            throw new IllegalDeviceParameterException(String.format("Unknown device parameter: %s", parameter));
-        }
+        return parameter.getSupportedClass();
     }
 
 }
